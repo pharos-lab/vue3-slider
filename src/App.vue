3d085389574ad237948a23cc0c6fb7839a215815
mthdht <mthdht@gmail.com>
2023-11-24 07:41:38 +0100

    made boilerplate for slider, create files

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,25 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="test">
+    <Slider>
+      <SLiderItem>1</SLiderItem>
+      <SLiderItem>2</SLiderItem>
+      <SLiderItem>3</SLiderItem>
+      <SLiderItem>4</SLiderItem>
+      <SLiderItem>5</SLiderItem>
+      <SLiderItem>6</SLiderItem>
+      <SLiderItem>7</SLiderItem>
+      <SLiderItem>8</SLiderItem>
+      <SLiderItem>9</SLiderItem>
+      <SLiderItem>10</SLiderItem>
+    </Slider>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
+<script setup>
+</script>
+
+
+
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
